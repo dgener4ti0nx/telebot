fdcb8a336b4ff5ccea3774ed92ed6269f28d7493
dgener4ti0nx <39700883+dgener4ti0nx@users.noreply.github.com>
2019-04-08 12:10:03 -0400

    Update telebot.sh

--- telebot.sh
+++ telebot.sh
@@ -16,7 +16,7 @@ else
 	echo "Sorry, receiver was not found."
 exit 1
 fi
- 
+echo "curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHATID"
 curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHATID -d text="$MESSAGE" -d parse_mode="markdown" #> /dev/null
 
 echo -e "Sent to $1: $MESSAGE"
