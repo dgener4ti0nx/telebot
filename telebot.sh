4f1209580994e0ebfcf54198862b06dee310e879
dgener4ti0nx <39700883+dgener4ti0nx@users.noreply.github.com>
2019-04-08 11:31:16 -0400

    Add files via upload

---
+++ telebot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+TOKEN=$(cat token)
+STDIN=$(cat)
+
+MESSAGE=$STDIN
+###################
+diego=773945556
+luiz=704337050
+###################
+
+if [ $1 == "diego" ]
+	then CHATID=$diego
+elif [ $1 == "luiz" ]
+	then CHATID=$luiz
+else
+	echo "Sorry, receiver was not found."
+exit 1
+fi
+
+curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHATID -d text="$MESSAGE" -d parse_mode="markdown" > /dev/null
+
+echo -e "Sent to $1: $MESSAGE"
+
